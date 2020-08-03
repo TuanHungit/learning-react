04d4487171ba17a079df865c8bed8bfc6699482b
NguyenTuanHung14 <56195033+NguyenTuanHung14@users.noreply.github.com>
2020-08-03 17:14:37 +0700

    list and key

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -4,9 +4,14 @@ import Person from './Person/Person';
 const app = (props)=>{
   const [personState, setPersonState] = useState({
     person:[
-      {name:"Tuan Hung"},
-      {name:"Tuan Kiet"}
+      {id:'1',name:"Tuan Hung"},
+      {id:'2',name:"Tuan Kiet"},
+      {id:'3',name:"Anh Dung"},
+      {id:'4',name:"Tuan Anh"}
     ]
+  });
+  const [ShowNameState, setShowNameState] = useState({
+    showName: false
   });
   const swichesName= ()=> {
     setPersonState({
@@ -16,25 +21,50 @@ const app = (props)=>{
       ]
     })
   }
-  const changedName = (event)=> {
+  const deletePerson = (personIndex)=>{
+    //bad code
+    //let persons = personState.person;
+    //Good practice
+    let persons = [...personState.person];
+    persons.splice(personIndex,1);
+    console.log(personState.person);
     setPersonState({
-      person:[ 
-      {name:"Tuan Hung"},
-      {name:event.target.value}
-    ]
-  })
+      person: persons
+    })
+  }
+  const showName = ()=>{
+    const toggleShowName = ShowNameState.showName;
+    setShowNameState({
+      showName: !toggleShowName
+    })
+  }
+  const changedName= (event,index)=>{
+    const person = {...personState.person[index]};
+    person.name = event.target.value;
+    const persons = [...personState.person];
+    persons[index] = person;
+    setPersonState({person:persons}); 
+  }
+  let persons = null;
+  if(ShowNameState.showName){
+    persons = (
+      <div>
+        {personState.person.map((person,index)=>{
+          return <Person 
+          name = {person.name} 
+          key = {person.id}
+          click={()=>deletePerson(index)}
+          changedName= {(event)=>changedName(event,index)}/>
+        })}
+      </div>
+    )
   }
   return (
     <div className="App">
       <h1>I'm React App !!!</h1>
-      <button onClick={swichesName}>Swiches name</button>
-      <Person name={personState.person[0].name}>My hobbies: music</Person>
-      <Person change={changedName} name={personState.person[1].name}/>
+      <button onClick={showName}>Swiches name</button>
+      {persons}
     </div>
   );
 }
- 
-    
-  
-
 export default app;
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -3,13 +3,11 @@ import './Person.css'
 const person = (props)=>{
 return (
     <div className="Person">
-         <p>I'm {props.name}</p>
+         <p  onClick={props.click}>I'm {props.name} </p>
          <p>{props.children}</p>
-         <input type="text" onChange={props.change} value={props.name}/>
+         <input type="text" value={props.name} onChange={props.changedName}/>
     </div>
-   
 ) 
-
 }
 
 export default person;
