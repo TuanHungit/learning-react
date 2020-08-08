2fa610de24260068315ea488e488043c770283a9
NguyenTuanHung14 <56195033+NguyenTuanHung14@users.noreply.github.com>
2020-08-08 15:44:42 +0700

    converted from functional component to class-base component

--- react-complete-guide/src/containers/App.js
+++ react-complete-guide/src/containers/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, Component } from 'react';
 import classes from './App.css';
 import Person from '../components/Persons/Person/Person';
 import Char from '../components/Char/Char';
@@ -6,105 +6,103 @@ import Persons from '../components/Persons/Person'
 //import Radium, {StyleRoot} from 'radium';
 import styled from 'styled-components';
 import Cockpit from '../components/Cockpit/Cockpit'
-const app = (props)=>{
-  const [personState, setPersonState] = useState({
-    person:[
-      {id:'1',name:"Tuan Hung", age:18},
-      {id:'2',name:"Tuan Kiet",age:20},
-      {id:'3',name:"Anh Dung",age:28},
-      {id:'4',name:"Tuan Anh",age:25}
-    ]
-  });
-  const [ShowNameState, setShowNameState] = useState({
-    showName: false
-  });
-  const swichesName= ()=> {
-    setPersonState({
+class app extends Component {
+  state = {
+      person:[
+        {id:'1',name:"Tuan Hung", age:18},
+        {id:'2',name:"Tuan Kiet",age:20},
+        {id:'3',name:"Anh Dung",age:28},
+        {id:'4',name:"Tuan Anh",age:25}
+      ],
+      showName: false,
+      value:''
+    };
+
+   swichesName= () => {
+    this.setState({
         person:[
         {name:"Tuan Hung Nguyen"},
         {name:"Tuan Kiet"}
       ]
     })
   }
-  const deletePerson = (personIndex)=>{
+  deletePerson = (personIndex)=>{
     //bad code
     //let persons = personState.person;
     //Good practice
-    let persons = [...personState.person];
+    let persons = [...this.state.person];
     persons.splice(personIndex,1);
-    console.log(personState.person);
-    setPersonState({
+    this.setState({
       person: persons
     })
   }
-  const showName = ()=>{
-    const toggleShowName = ShowNameState.showName;
-    setShowNameState({
+  showName = ()=>{
+    const toggleShowName = this.state.showName;
+    this.setState({
       showName: !toggleShowName
     })
   }
-  const changedName= (event,index)=>{
-    const person = {...personState.person[index]};
+  changedName= (event,index)=>{
+    const person = {...this.state.person[index]};
     person.name = event.target.value;
-    const persons = [...personState.person];
+    const persons = [...this.state.person];
     persons[index] = person;
-    setPersonState({person:persons}); 
+    this.setState({person:persons}); 
   }
 
-  let persons = null;
-
-  if(ShowNameState.showName){
-    persons = (
-      <Persons persons = {personState.person}
-      deletePerson = {deletePerson}
-      changedName = {changedName}/>
-    )
-
-  }
+ 
   //---------------------Assignment  
-  const [InputState, setInputState] = useState({
-    value:''
-  });
-  const changeInput = (event)=>{
-    setInputState({
+  changeInput = (event)=>{
+    this.setState({
       value: event.target.value
     })
   }
-  const changeName = (event)=>{
-    setInputState({
+  changeName = (event)=>{
+    this.setState({
       value: event.target.value
     })
   }
-  const charList = InputState.value.split('').map((el,index)=>{
+  charList = this.state.value.split('').map((el,index)=>{
     return <Char 
     char={el} 
     key={index}
-    click ={()=>deleteChar(index)}/>
+    click ={()=>this.deleteChar(index)}/>
   })
-  const deleteChar = (index)=>{
-    const charList = [...InputState.value.split('')];
+  deleteChar = (index)=>{
+    const charList = [...this.state.value.split('')];
     charList.splice(index,1);
     
     const updated = charList.join('');
     console.log(updated);
-    setInputState({
+    this.setState({
       value: updated
     })
     
   }
  
-  
-  return (
+  render(){
+    let persons = null;
+
+    if(this.state.showName){
+      persons = (
+        <Persons persons = {this.state.person}
+        deletePerson = {this.deletePerson}
+        changedName = {this.changedName}/>
+      )
+
+    }
+    return (
       <div className={classes.App}>
-       <Cockpit persons = {personState}
-       showName = {ShowNameState.showName}
-       clicked = {showName}/>
+       <Cockpit persons = {this.state.person}
+       showName = {this.state.showName}
+       clicked = {this.showName}/>
        {persons}
 
         {/* <input type="text" onChange={changeInput} value={InputState.value}/>
         <p>{charList}</p>
         <Validation text = {charList} /> */}
       </div>
-  );
+    );
+  }
 }
 export default app;
