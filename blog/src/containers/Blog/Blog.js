d6b50f41763d96d92ae062aee591dc2c474a05d1
NguyenTuanHung14 <56195033+NguyenTuanHung14@users.noreply.github.com>
2020-08-17 20:48:52 +0700

    render fetched data to screen

--- blog/src/containers/Blog/Blog.js
+++ blog/src/containers/Blog/Blog.js
@@ -1,28 +1,47 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
-    render () {
-        return (
-            <div>
-                <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
-                </section>
-                <section>
-                    <FullPost />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
-            </div>
-        );
-    }
+  state = {
+    posts: [],
+    count: 0,
+  };
+  componentWillMount() {
+    console.log('[componentWillMount]');
+  }
+  componentDidMount() {
+    console.log('[componentDidMount]');
+    axios.get('https://jsonplaceholder.typicode.com/posts').then((response) => {
+      const updatedPost = response.data.slice(0, 4).map((post) => {
+        return { ...post, author: 'Hung' };
+      });
+      this.setState({
+        posts: updatedPost,
+      });
+    });
+  }
+  render() {
+    console.log(`render`);
+    console.log(this.state);
+    const posts = this.state.posts.map((post) => {
+      return <Post key={post.id} title={post.title} author={post.author} />;
+    });
+    return (
+      <div>
+        <section className='Posts'>{posts}</section>
+        <section>
+          <FullPost />
+        </section>
+        <section>
+          <NewPost />
+        </section>
+      </div>
+    );
+  }
 }
 
 export default Blog;
