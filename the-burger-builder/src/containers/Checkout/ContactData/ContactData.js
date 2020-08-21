59733aa8bf3e15b88226c9ec2555595a72989222
NguyenTuanHung14 <56195033+NguyenTuanHung14@users.noreply.github.com>
2020-08-21 22:16:43 +0700

    setting up a config for the form

--- the-burger-builder/src/containers/Checkout/ContactData/ContactData.js
+++ the-burger-builder/src/containers/Checkout/ContactData/ContactData.js
@@ -4,16 +4,62 @@ import Button from '../../../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import classes from './ContactData.css';
 import axios from '../../../axios-orders';
-
+import Input from '../../../components/UI/Input/Input';
 class ContactData extends Component {
   state = {
-    name: '',
-    email: '',
-    address: {
-      street: '',
-      postalCode: '',
+    orderForm: {
+      name: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Your Name',
+        },
+        value: '',
+      },
+      street: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Street',
+        },
+        value: '',
+      },
+      zipCode: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'ZIP Code',
+        },
+        value: '',
+      },
+      country: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Country',
+        },
+        value: '',
+      },
+      email: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Your E-mail',
+        },
+        value: '',
+      },
+      deliveryMethod: {
+        elementType: 'select',
+        elementConfig: {
+          options: [
+            { value: 'fastest', displayValue: 'Fastest' },
+            { value: 'cheapest', displayValue: 'Cheapest' },
+          ],
+        },
+        value: '',
+      },
+      loading: false,
     },
-    loading: false,
   };
 
   orderHandler = (event) => {
@@ -45,32 +91,24 @@ class ContactData extends Component {
   };
 
   render() {
+    const formElementArrar = [];
+    for (let key in this.state.orderForm) {
+      formElementArrar.push({
+        id: key,
+        config: this.state.orderForm[key],
+      });
+    }
+
     let form = (
       <form>
-        <input
-          className={classes.Input}
-          type='text'
-          name='name'
-          placeholder='Your Name'
-        />
-        <input
-          className={classes.Input}
-          type='email'
-          name='email'
-          placeholder='Your Mail'
-        />
-        <input
-          className={classes.Input}
-          type='text'
-          name='street'
-          placeholder='Street'
-        />
-        <input
-          className={classes.Input}
-          type='text'
-          name='postal'
-          placeholder='Postal Code'
-        />
+        {formElementArrar.map((formElement) => (
+          <Input
+            key={formElement.id}
+            elementType={formElement.config.elementType}
+            elementConfig={formElement.config.elementConfig}
+            value={formElement.config.value}
+          />
+        ))}
         <Button btnType='Success' clicked={this.orderHandler}>
           ORDER
         </Button>
